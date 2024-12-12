d8cc5e60fb5ccc39e58b7cc4d9d2b41fa4d14fdb
Bui Trong Van <trongvan159357@gmail.com>
2024-12-12 13:48:21 +0700

    update matrix

--- content/data-structures/Matrix.h
+++ content/data-structures/Matrix.h
@@ -38,30 +38,19 @@ template<class T, int N> struct Matrix {
 		return a;
 	}
 };
-
-
-
+//
 using type = int; // Kiểu dữ liệu các phần tử của ma trận
-
 struct Matrix {
     vector <vector <type> > data;
-
     // Số lượng hàng của ma trận
     int row() const { return data.size(); }
-
     // Số lượng hàng của ma trận
     int col() const { return data[0].size(); }
-
     auto & operator [] (int i) { return data[i]; }
-
     const auto & operator[] (int i) const { return data[i]; }
-
     Matrix() = default;
-
     Matrix(int r, int c): data(r, vector <type> (c)) { }
-
     Matrix(const vector <vector <type> > &d): data(d) {
-
         // Kiểm tra các hàng có cùng size không và size có lớn hơn 0 hay không
         // Tuy nhiên không thực sự cần thiết, ta có thể bỏ các dòng  đi
          assert(d.size());
@@ -69,7 +58,6 @@ struct Matrix {
          assert(size);
          for (auto x : d) assert(x.size() == size);
     }
-
     // In ra ma trận.
     friend ostream & operator << (ostream &out, const Matrix &d) {
         for (auto x : d.data) {
@@ -78,21 +66,17 @@ struct Matrix {
         }
         return out;
     }
-
     // Ma trận đơn  vị
     static Matrix identity(long long n) {
         Matrix a = Matrix(n, n);
         while (n--) a[n][n] = 1;
         return a;
     }
-
     // Nhân ma trận
     Matrix operator * (const Matrix &b) {
         Matrix a = *this;
-
         // Kiểm tra điều kiện nhân ma trận
         assert(a.col() == b.row());
-
         Matrix c(a.row(), b.col());
         for (int i = 0; i < a.row(); ++i)
             for (int j = 0; j < b.col(); ++j)
@@ -100,55 +84,44 @@ struct Matrix {
                     c[i][j] += a[i][k] * b[k][j];
         return c;
     }
-
     // Lũy thừa ma trận
     Matrix pow(long long exp) {
-
         // Kiểm tra điều kiện lũy thừa ma trận (là ma trận vuông)
         assert(row() == col());
-
         Matrix base = *this, ans = identity(row());
         for (; exp > 0; exp >>= 1, base = base * base)
             if (exp & 1) ans = ans * base;
         return ans;
     }
 };
-
 int main(){
     Matrix a({
         {1, 2},
         {3, 4}
     });
-
     Matrix b({
         {0, 10, 100},
         {1,  1,  10}
     });
-
     cout << a * b << '\n';
     // 2 12 120
     // 4 34 340
-
     cout << a.pow(3) << '\n';
     // 37 54
     // 81 118
-
     b = a;
     cout << b << '\n';
     // 1 2
     // 3 4
-
     b = Matrix::identity(3);
     cout << b << '\n';
     // 1 0 0
     // 0 1 0
     // 0 0 1
-
     b = Matrix(2, 3);
     cout << b << '\n';
     // 0 0 0
     // 0 0 0
-
     Matrix c(3, 2);
     cout << c << '\n';
     // 0 0
